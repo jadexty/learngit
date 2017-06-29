cf204981981a32e532a26844d2fc506c65013f53
jhtianye <hz.tianye@gmail.com>
2017-06-29 22:08:27 +0800

    click on a line

--- vim/vim80/py_ws/byl/byl_1.py
+++ vim/vim80/py_ws/byl/byl_1.py
@@ -5,11 +5,21 @@
 #------------------------------
 import autopy
 import time
-#autopy.mouse.move(100, 100) 
-autopy.mouse.move(957, 695) 
-time.sleep(3)
-autopy.mouse.click
-autopy.mouse.move(100, 100) 
 
+#autopy.mouse.move(957, 695) 
+#time.sleep(1/3)
+#autopy.mouse.click
 
+#让鼠标在一条竖线上一边点击，一边移动
+#(920, 740),(920, 680)
+
+i = 740 
+print(i)
+while i >680 :
+    autopy.mouse.move(920, i) 
+    print ("i:  ",i)
+    time.sleep(1/2)
+    autopy.mouse.click
+    time.sleep(1/2)
+    i = i - 1 
 
