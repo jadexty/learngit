e8fb588532f90ade4838e8dee97045720a7190be
jhtianye <hz.tianye@gmail.com>
2017-06-29 20:53:18 +0800

    add byl_1.py

---
+++ vim/vim80/py_ws/byl/byl_1.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+#捕鱼来了辅助
+#description：
+#用autopy模拟鼠标移动，实现自动攻击
+#------------------------------
+import autopy
+import time
+#autopy.mouse.move(100, 100) 
+autopy.mouse.move(957, 695) 
+time.sleep(3)
+autopy.mouse.click
+autopy.mouse.move(100, 100) 
+
+
+
