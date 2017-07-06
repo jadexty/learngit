f5209328eabd5d7994271d7e52b57a212bd5d339
jadexty <hz.tianye@gmail.com>
2017-07-07 00:49:25 +0800

    baocun hotkey_runPy.py

--- vim/vim80/byl/hotkey_runPy.py
+++ vim/vim80/byl/hotkey_runPy.py
@@ -10,79 +10,21 @@ import ctypes
 from ctypes import wintypes
 import win32con
 import win32api 
+import subprocess
+py_file = 'byl_autopy.py'
 
-py_file = 'byl_mouse.py'
-class CrunPy:
-    '''
-    Function:键盘和鼠标监控类
-    Input：NONE
-    Output: NONE
-    author: socrates
-    blog:http://blog.csdn.net/dyx1024
-    date:2012-03-09
-    ''' 
-    def __init__(self, filename):
-        '初始化'
-        self.filename = filename
-        
-    def run_py(self):
-        '运行文件'
-        os.system("python "+ py_file)
-        
-    def kill_py(self):
-        '退出程序'
-        os.system("taskkill /im " + py_file )
-        
-    def IsExitCommand(self, event):
-        '''
-                     是否当前按下了程序定义的热键'
-                      如果按下了ALT+F2，
-                     如果按下了ALT+F1，
-        '''
-        if event.Alt == 32 and str(event.Key) == 'F2':
-            print 'F2 true'
-            self.kill_py()
-        elif  event.Alt == 32 and str(event.Key) == 'F1':  
-            print 'F1 true'
-            self.run_py()
-        
-    
-    def onKeyboardEvent(self, event): 
-        
-        print #处理按下的热键
-        self.IsExitCommand(event)
-        
-    
-            
-        
-
-def InspectKeyAndMouseEvent():
-    "启动监控"
-    my_event = CrunPy(py_file)
-    my_event.run_py()
-     
-    #创建hook句柄
-    hm = pyHook.HookManager()
-    
-    #监控键盘
-    hm.KeyDown = my_event.onKeyboardEvent
-    hm.HookKeyboard()
-    
-    #监控鼠标
-    #hm.MouseAll = my_event.onMouseEvent
-    #hm.HookMouse()
-    
-    #循环获取消息
-    pythoncom.PumpMessages()
-    my_event.kill_py()             
-     
-def handle_start_InspecEvent():
-    print "开始监控（按下ALT+ F1）"
-    InspectKeyAndMouseEvent()
-
-def handle_stop_InspecEvent():
-    print"停止监控  (按下ALT+ F2)"
-    InspectKeyAndMouseEvent(False)   
+def begin_byl():
+    child = subprocess.Popen(["byl_autopy.bat"])
+  
+def kill_byl():
+    "开始监控（按下ALT+ F1）"
+    print "kill vimrc.exe"
+    #os.system("taskkill /im vimrc.exe /f")
+    #child = subprocess.Popen(["taskkill /im vimrun.exe /f"])
+    sys.exit()
+#def handle_stop_InspecEvent():
+#    print"停止监控  (按下ALT+ F2)"
+#    InspectKeyAndMouseEvent(False)   
         
           
 if __name__ == "__main__":     
@@ -105,13 +47,10 @@ if __name__ == "__main__":
     print HOTKEYS
     #快捷键对应的驱动函数
     HOTKEY_ACTIONS = {
-        1 : handle_start_InspecEvent,
-        2 : handle_stop_InspecEvent
+        1 : begin_byl, 
+        2 : kill_byl
         }    
 
-    print "HOTKEY_ACTIONS ->" 
-    print type(HOTKEY_ACTIONS) 
-    print HOTKEY_ACTIONS
     #注册快捷键
     for id, (vk, modifiers) in HOTKEYS.items ():
         if not user32.RegisterHotKey (None, id, modifiers, vk):
