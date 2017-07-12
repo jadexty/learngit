367244ef2892e3fc469b2e27000eb17a8eb2aac1
jadexty <hz.tianye@gmail.com>
2017-07-12 22:33:45 +0800

    has_pic.py

--- vim/vim80/byl/byl_main.py
+++ vim/vim80/byl/byl_main.py
@@ -1,7 +1,6 @@
 #coding=utf-8
 #------------------------
-#主线程sys.exit, 子线程byl_run不会停止.
-#把它作为deamon 线程
+#主线程sys.exit, 守护线程byl_run也会停止.
 #------------------------
 import subprocess
 import autopy
@@ -10,14 +9,25 @@ import os,sys
 import threading
 from threading import Thread
 import ctypes, win32con, ctypes.wintypes, win32gui
+from PIL import Image,ImageGrab
+def kill_child():
+            os.system("taskkill /im AppMarket.exe /f")
+            os.system("taskkill /im  AndroidEmulator.exe /f")
+            os.system("taskkill /im  adb.exe /f")
+            child.kill()
+            child = subprocess.Popen("python byl_run.py",shell=True)
+
 def pos_col(pos,cols):
     "每过一秒,将pos位置的color放入cols"
     n = 0
     while n < len(cols):
         #print "n-->{} \n".format(n)
-        col = autopy.bitmap.capture_screen().get_color(pos[0], pos[1]) 
+        #col = autopy.bitmap.capture_screen().get_color(pos[0], pos[1]) 
+        "打印屏幕" 
+        pic = ImageGrab.grab()
+        col = pic.getpixel((pos[0],pos[1]))
+        #print 'col: {} \n'.format(col)
         cols[n] = col
-        #print "col -->{} \n".format(col)
         n = n + 1
         time.sleep(1)
     print "pos:{}-->cols: {} \n".format(pos,cols)
@@ -62,61 +72,64 @@ def if_diff():
     每过一秒钟取pos位置的颜色,一共60秒,判断这60个颜色数值是否一样,
     如果一样就表示游戏已经停止了.
     ''' 
-    "问题:60秒取色也可能会一样,原因不明"
     while True:
         cols = list(range(60)) 
         pos_2 = (811, 571)
         pos_1 = (813, 762)
         "只要有一个是true,就表示画面没有停止"
+        print 'pos_col(pos_1,cols): {}'.format(pos_col(pos_1,cols))
+        print 'pos_col(pos_2,cols): {}'.format(pos_col(pos_2,cols))
         if pos_col(pos_1,cols) == True or pos_col(pos_2,cols) == True:
-            return True
-        return False
+           continue 
+        kill_child() 
+
+
 def byl_run(a):
-        import byl_run
+    "导入byl_run"
+    import byl_run
+    #from byl_run import shooting
 if __name__ == '__main__':
+    "设置热键,设置线程退出标识为False"
     EXIT = False
     hotkey = Hotkey()
     hotkey.start()
-    "线程类是没有join方法的,线程函数有"
+    "线程class是没有join方法的,线程function有"
     #Hotkey.join()
 
-    #t = Thread(target=countdown, args=(10,), daemon=True)
+    "守护线程方式启动byl"
     th_byl=Thread(target=byl_run,args=(10,))  
     th_byl.setDaemon(True)   
     th_byl.start()
     #child = subprocess.Popen("python byl_run.py > byl_run_log",shell=True)
     #child = subprocess.Popen("python byl_run.py ",shell=True)
+    
+    "画面停止,就kill所有子进程,重新开始."
+    th_ifDiff = Thread(target=if_diff)
+    th_ifDiff.daemon = True
+    th_ifDiff.start()
 
-    child = subprocess.Popen("shutdown -r -t 3600",shell=True)
-    print'EXIT == False'
-    while True :
-        if EXIT == True:
-            print 'EXIT == True'
-            sys.exit(0)
+    "一个小时后重新启动电脑"
+    child = subprocess.Popen("shutdown -r -t 7200",shell=True)
 
-    #child = subprocess.Popen("shutdown -r -t 60",shell=True)
-#
-#    begin_time = time.time()
-#    print ('begin_time:%s'%begin_time)
-#    "画面停止,就kill所有子进程,重新开始子进程."
-#    def kill_child():
-#            os.system("taskkill /im AppMarket.exe /f")
-#            os.system("taskkill /im  AndroidEmulator.exe /f")
-#            os.system("taskkill /im  adb.exe /f")
-#            #child.kill()
-#            child = subprocess.Popen("python byl_run.py",shell=True)
-#    while True:
-#        print ('main if_diff: %s'%if_diff())
-#        if if_diff() == False:
-#           kill_child() 
-#
-#        "byl_run进行多少时间后,就关闭所有的子进程, 重新开始"
-#        set_time = 60*60
-#        #set_time = 10 
-#        end_time = time.time() 
-#        exp_time = end_time - begin_time  
-#        print ('end_time: %s'%end_time)
-#        print ('exp_time: %s'%exp_time)
-#        print ('set_time: %s'%set_time)
-#        while exp_time > set_time:
-#            kill_child()
+    "byl_run进行多少时间后,就关闭所有的子进程, 重新开始"
+    print 'exp_time will start'
+    begin_time = time.time()
+    print ('begin_time:%s'%begin_time)
+    set_time = 60*60
+    #set_time = 10 
+    print ('set_time: %s'%set_time)
+    end_time = time.time() 
+    exp_time = end_time - begin_time  
+    while True:
+        end_time = time.time() 
+        exp_time = end_time - begin_time  
+        #print ('end_time: %s'%end_time)
+        print ('exp_time: %s'%exp_time)
+        print ('set_time: %s'%set_time)
+        print 'exp_time > set_time: {}'.format(exp_time > set_time)
+        time.sleep(20)
+        while exp_time > set_time:
+            end_time = time.time() 
+            exp_time = end_time - begin_time  
+            kill_child()
+        
